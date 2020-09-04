93964bdd86b31a6f6799d8ad9ed4220f4fba0a74
AdrianaGavinoDias <70656828+AdrianaGavinoDias@users.noreply.github.com>
2020-09-04 22:25:52 +0200

    Add files via upload
    Another version, fewer lines: don't know if it'll work

--- P2Question14.m
+++ P2Question14.m
@@ -29,16 +29,12 @@ end
 end
 %chisq=-2*Loglike
 %chisq=chisq-min(chisq)
-figure(2)
+figure(1)
 xlabel("w0")
 ylabel("Omega M")
 %colormap(w0,Omm,chisq)
 contour(w0, Omm, Loglike, [max(max(Loglike)) - 5.9, max(max(Loglike)) - 3.1, max(max(Loglike)) - 1.15])
 H01=68;
-N = 5;
-Omm = 0 : 1/N : 1;
-Oml = 1 - Omm;
-w0 = -2 : 1/(N/2) : 0;
 Loglike1 = zeros(N + 1, N + 1);
 for j = 1 : N + 1
 for l = 1 : N + 1
@@ -55,18 +51,14 @@ Loglike1(j, l) = Loglike1(j, l) - (1/2)*(DLth1 - DL(i)).^2/errDL(i).^2;
 end
 end
 end
-%chisq=-2*Loglike
-%chisq=chisq-min(chisq)
-figure(2)
+%chisq1=-2*Loglike1
+%chisq1=chisq1-min(chisq1)
+figure(1)
 xlabel("w0")
 ylabel("Omega M")
-%colormap(w0,Omm,chisq)
+%colormap(w0,Omm,chisq1)
 contour(w0, Omm, Loglike1, [max(max(Loglike1)) - 5.9, max(max(Loglike1)) - 3.1, max(max(Loglike1)) - 1.15])
 H02=68;
-N = 5;
-Omm = 0 : 1/N : 1;
-Oml = 1 - Omm;
-w0 = -2 : 1/(N/2) : 0;
 Loglike2 = zeros(N + 1, N + 1);
 for j = 1 : N + 1
 for l = 1 : N + 1
@@ -83,11 +75,11 @@ Loglike2(j, l) = Loglike2(j, l) - (1/2)*(DLth2 - DL(i)).^2/errDL(i).^2;
 end
 end
 end
-%chisq=-2*Loglike
-%chisq=chisq-min(chisq)
-figure(2)
+%chisq2=-2*Loglike2
+%chisq2=chisq2-min(chisq2)
+figure(1)
 xlabel("w0")
 ylabel("Omega M")
-%colormap(w0,Omm,chisq)
+%colormap(w0,Omm,chisq2)
 contour(w0, Omm, Loglike2, [max(max(Loglike2)) - 5.9, max(max(Loglike2)) - 3.1, max(max(Loglike2)) - 1.15])
 print -dpng P2Q14
